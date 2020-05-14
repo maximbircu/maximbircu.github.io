2629208c5a0af4a24884e083c411413f6f7957ca
Maxim Bîrcu <maximbircu@gmail.com>
2020-05-14 20:18:17 +0300

    [Post] Introduction to machine learning (#24)
    * Write introduction to machine learning post
    
    * Move neural networks post to the drafts folder
    
    * Fix post styles
    
    * Set autoscroll speed to 1s
    
    * Make sections headings uppercase

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function ($) {
     'use strict';
     let sections = $('.sections-wrapper');
-    sections.stop().animate({scrollTop: cookie.get('post-scroll')}, 500, 'swing', null);
+    sections.stop().animate({scrollTop: cookie.get('post-scroll')}, 1000, 'swing', null);
     sections.scroll(function () {
         cookie.set('post-scroll', sections.scrollTop());
     });
